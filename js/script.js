7be6ad42f3f66c91e6c616cfbcd322815cda0101
MoisesEastland1 <meastland977@gmail.com>
2025-10-01 18:41:59 -0500

    added page router

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const global = {
+  currentPage: window.location.pathname
+}
+
+//Init App
+function init() {
+  switch(global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+    break;
+    case '/shows.html':
+      console.log('Shows');
+    break;
+    case '/movie-details.html':
+      console.log('Movies Details');
+    break;
+    case '/tv-details.html':
+      console.log('TV Details');
+    break;
+    case '/search.html':
+      console.log('Search');
+    break;
+
+
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
