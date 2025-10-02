6834725409704548941565b86b3646577bd43385
MoisesEastland1 <meastland977@gmail.com>
2025-10-02 17:46:18 -0500

    add displayPopShows funct

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function hightlightALink() {
   });
 };
 
+  // display 20 most popular movies
 async function displayPopMovies() {
   const {results}  = await fetchAPIData('movie/popular');
   results.forEach((movie => {
@@ -34,7 +35,7 @@ async function displayPopMovies() {
     </a>
 
     <div class="card-body">
-    <h5 class="card-title">${movie.original_title}</h5>
+    <h5 class="card-title">${movie.title}</h5>
     <p class="card-text">
     <small class="text-muted">Release: ${movie.release_date}</small>
     </p>
@@ -42,6 +43,40 @@ async function displayPopMovies() {
     `;
 
     document.querySelector('#popular-movies').appendChild(div);
+  }));
+}
+
+  // display 20 most popular TV shows
+async function displayPopShows() {
+  const {results}  = await fetchAPIData('tv/popular');
+  results.forEach((show => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    
+    <a href="tv-details.html?id=${show.id}">
+    ${show.poster_path
+    ? 
+      `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"/>`
+
+    : 
+    
+    `<img src="..images/noImage.jpg" class="card-img-top"
+    alt="${show.name}"/>`
+    }
+    </a>
+
+    <div class="card-body">
+    <h5 class="card-title">${show.name}</h5>
+    <p class="card-text">
+    <small class="text-muted">Air Date: ${show.first_air_date}</small>
+    </p>
+    </div>
+    `;
+
+    document.querySelector('#popular-shows').appendChild(div);
   }));
 }
 
@@ -77,7 +112,7 @@ function init() {
       displayPopMovies();
     break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopShows();
     break;
     case '/movie-details.html':
       console.log('Movies Details');
