70ce695fa2b42601cc6b621651d354653d1226d2
MoisesEastland1 <meastland977@gmail.com>
2025-10-04 14:53:16 -0500

    added custom alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const global = {
-  currentPage: window.location.pathname
+  currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1
+  }
 }
 
 //Highlight active link
@@ -11,6 +17,16 @@ function hightlightALink() {
     }
   });
 };
+
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div'); 
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
 
 function addCommasToNumbers(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -252,6 +268,21 @@ function displayBackgroundImage
   } else {
     document.querySelector('#show-details').appendChild(overLayDiv);
   }
+};
+
+// Search Movies/Shows
+async function searchMovieShow() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+    if(global.search.term !== '' && global.search.term !== null) {
+    // @todo - make request and display results
+  } else {
+    showAlert('Please enter a search term');
+  }
 }
 
 // Display Slider Movies
@@ -346,7 +377,7 @@ function init() {
       displayShowDetails();
     break;
     case '/search.html':
-      console.log('Search');
+      searchMovieShow();
     break;
   }
 
